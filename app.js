e53b864a99c4fa7bcfe69c40e1a5855bdbcf03b0
mikiyashussen <mikiyashussen19@gmail.com>
2022-12-29 14:48:34 +0100

    made draft plan for routes

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
-
-
 const app = express();
 const port = 5000;
-console.log("whattt ")
+
+
+
+// decide routes
+
+//1. /home
 app.get('/', (req, res) => {
     console.log("heloooo.")
-    res.send("hellooooo")
+    res.send("/Home")
 });
+
+//2. /category
+app.get('categories', (req, res) => {
+    // fetch categories for dsb
+    // const categories = db.categories.find(); so to do this I need to have a databse first
+    // send obj to client
+    // res.send(categories)
+})
+//3. /category:id
+
+//4. /buynow
+
+//5. /cart
+
+//6. /payment
+
+//7. /Auth
 
 
 app.listen(port, () => {
